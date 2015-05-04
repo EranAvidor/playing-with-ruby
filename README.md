672046d89660239ca65b5adab49ba241f1ad79e8
Eran <eran@Udis-MacBook-Pro.local>
2015-05-04 22:48:39 +0300

    colored output text added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Test
+Colors added to output text
--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
+require 'colorize'
+
 def add a, b
   a+b
 end
 
-puts 'Enter first number'
+puts 'Enter first number'.blue
 input1 = gets
-puts 'Enter second number'
+puts 'Enter second number'.blue
 input2 = gets
 
-puts "The result is:"
-puts "#{add input1.to_i, input2.to_i}"
+puts "The result is:".green
+puts "#{add input1.to_i, input2.to_i}".green
