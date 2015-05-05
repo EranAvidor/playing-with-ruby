49404f2e6241001c150cfa8004b48a43de2001cb
Eran <eran@Udis-MacBook-Pro.local>
2015-05-05 12:01:20 +0300

    cases.rb added - testing cases statement

---
+++ cases.rb
@@ -0,0 +1,13 @@
+#using case when - switch case statement
+
+puts "Tell me a car model: "
+car_model = gets.strip #strip removes any sapces from the beginning and the end of string
+
+output = case car_model
+			when "Focus", "Fieasta" then "Ford"
+			when "Ibiza" then "Seat"
+			when "Civic" then "Honda"
+			else "Unknown model"	#else is optional and is a sort of a backfall
+			end
+
+puts "The car company for #{car_model} is ", output
