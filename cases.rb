b082225557b3041ecf1609de79df9d8e05329a58
Eran <eran@Udis-MacBook-Pro.local>
2015-05-05 12:52:57 +0300

    cases.rb - spelling errors were corrected

--- cases.rb
+++ cases.rb
@@ -1,13 +1,13 @@
 #using case when - switch case statement
 
 print "Tell me a car model: "
-car_model = gets.strip #strip removes any sapces from the beginning and the end of string
+car_model = gets.strip #strip removes any spaces from the beginning and the end of string
 
 output = case car_model
 			when "Focus", "Fieasta" then "Ford"
 			when "Ibiza" then "Seat"
 			when "Civic" then "Honda"
-			else "Unknown model"	#else is optional and is a sort of a backfall
+			else "Unknown model"	#else is optional and is a sort of a back-fall
 			end
 
 print "The car company for #{car_model} is ", output
