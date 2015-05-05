e1eee7c2c86f5712fb0f40aee2d1273c915e7761
Eran <eran@Udis-MacBook-Pro.local>
2015-05-05 11:45:23 +0300

    age.rb changed - output with a variable

--- age.rb
+++ age.rb
@@ -4,17 +4,17 @@ age = gets.to_i
 
 
 #2. process
-if age < 10
-  puts "It's a young person."
-elsif age < 19
-  puts "It's a teenager."
-elsif age < 55
-  puts "It's an adult."
-elsif age < 100
-  puts "It's an old person."
-else
-  puts "Is he dead?"
-end
-
+output = if age < 10
+	       "It's a young person."
+	     elsif age < 19
+           "It's a teenager."
+		 elsif age < 55
+		   "It's an adult."
+		 elsif age < 100
+		   "It's an old person."
+	 	 else
+	 	   "Is he dead?"
+		 end
 
 #3. output
+puts output
