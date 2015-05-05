82d208247d865b839fca3672547c5755e9ec5bf4
Eran <eran@Udis-MacBook-Pro.local>
2015-05-05 11:23:13 +0300

    add_and_power.rb added

---
+++ add_and_power.rb
@@ -0,0 +1,11 @@
+def add_and_power a, b
+  (a + b) ** (a + b)
+end
+
+puts 'Enter first number'
+input1 = gets
+puts 'Enter second number'
+input2 = gets
+
+puts "The result is:"
+puts "#{add_and_power input1.to_i, input2.to_i}"
